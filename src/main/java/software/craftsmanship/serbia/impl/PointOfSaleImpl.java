70abb5e393469bc098f476b649a945dbc2200ad5
Filip Petrovic <fpetrovic92@gmail.com>
2018-04-18 18:49:38 +0200

    Add PointOfSaleImpl

---
+++ src/main/java/software/craftsmanship/serbia/impl/PointOfSaleImpl.java
@@ -0,0 +1,22 @@
+package software.craftsmanship.serbia.impl;
+
+import software.craftsmanship.serbia.PointOfSale;
+
+public class PointOfSaleImpl implements PointOfSale {
+
+
+    @Override
+    public void onBarcode(String barcode) {
+
+    }
+
+    @Override
+    public String total() {
+        return null;
+    }
+
+    @Override
+    public void onBarcodeRemove(String barcode) {
+
+    }
+}
